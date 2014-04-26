29b69e15f1c8d3562632aff954a75caa79387714
Rick Eyre <rick.eyre@hotmail.com>
2014-04-25 20:20:00 -0400

    Implement property type validation.

--- test/validator.js
+++ test/validator.js
@@ -4,6 +4,8 @@
 var validator = require("../lib/validator"),
     _ = require("lodash"),
     expect = require("chai").expect,
+    path = require("path"),
+    specification = require(path.resolve(__dirname, "../lib/specification.json")),
     mapping = { };
 
 suite("validator", function() {
@@ -22,10 +24,37 @@ suite("validator", function() {
 
   test("command and alias should be required properties", function() {
     expect(validator(mapping)).to.have.length(2);
-    expect(validator({ "command": "git" })).to.have.length(1);
-    expect(validator({ "alias": "g" })).to.have.length(1);
+    expect(validator({ command: "git" })).to.have.length(1);
+    expect(validator({ alias: "g" })).to.have.length(1);
     _.assign(mapping, { command: "git", alias: "g"});
     expect(validator(mapping)).to.have.length(0);
+  });
+
+  test("type checking should work correctly", function() {
+    var base = { command: "git", alias: "g" },
+        types = {
+          string: "string",
+          number: 1,
+          "function": function() {},
+          object: {},
+          array: []
+        };
+
+    // Loop through each valid property in the specification.
+    _.forEach(specification, function(propertySpec, propertyKey) {
+      // Find the type that the property is specified to be. Default to string.
+      var type = _.find(propertySpec, "type") || "string";
+      // Test that each type that is not the specified type returns an error.
+      _.forEach(_.omit(types, type), function(badType) {
+        var cp = _.clone(base);
+        cp[propertyKey] = badType;
+        expect(validator(cp)).to.have.length(1);
+      });
+      // Test that the type that it is specified to be does not return an error.
+      var cp = _.clone(base);
+      cp[propertyKey] = types[type];
+      expect(validator(cp)).to.have.length(0);
+    });
   });
 
 });
